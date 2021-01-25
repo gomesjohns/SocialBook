4d8d88af872779096e9f146ef6a0dac64e65f2b2
John Gomes <gomes.john103@gmail.com>
2021-01-24 19:18:08 -0500

    Implemented wall post feature

--- socialbook/src/components/Feed.js
+++ socialbook/src/components/Feed.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./Feed.css";
 import StoryReel from "./StoryReel";
 import WallPosts from "./WallPosts";
+import Post from "./Post";
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
       <WallPosts />
-      {/* StoryReel */}
+
+      <Post
+      /* profilePic={pic}
+        message={message}
+        timestamp={timestamp}
+        username={username}
+        image={image} */
+      />
+      <Post />
+      <Post />
     </div>
   );
 }
--- socialbook/src/components/WallPosts.js
+++ socialbook/src/components/WallPosts.js
@@ -1,8 +1,60 @@
-import React from "react";
+import { Avatar } from "@material-ui/core";
+import React, { useState } from "react";
 import "./WallPosts.css";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 function WallPosts() {
-  return <div className="wallPosts"></div>;
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //DB
+
+    setInput("");
+    setImageUrl("");
+  };
+  return (
+    <div className="wallPosts">
+      <div className="wallPosts__top">
+        <Avatar />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="wallPosts_input"
+            placeholder={"What's on your mind?"}
+          />
+          <input
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            placeholder={"Image URL (Optional)"}
+          />
+
+          <button onClick={handleSubmit} type="submit">
+            Hidden Submit
+          </button>
+        </form>
+      </div>
+      <div className="wallPosts__bottom">
+        <div className="wallPosts__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="wallPosts__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/ Video</h3>
+        </div>
+        <div className="wallPosts__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h3>Feeling/ Activity</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WallPosts;
